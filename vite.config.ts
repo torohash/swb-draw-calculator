6ac8ceffe135022c0d976d1a7ab546531b211a7b
torohash <torohash@gmail.com>
2025-06-23 11:00:23 +0000

    パス修正

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from "vite";
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [react()],
+	base: "/swb-draw-calculator/",
 	server: {
 		port: 3000,
 		strictPort: true, // ポートが被っていたらエラー
